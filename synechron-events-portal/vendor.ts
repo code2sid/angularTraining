812a69094d5ac69b5e1480ea8ccdc9731fc31767
code2sid <writ2siddharthg@gmail.com>
2019-01-15 17:40:46 +0530

    events project

---
+++ synechron-events-portal/vendor.ts
@@ -0,0 +1,12 @@
+import "@angular/core";
+import "@angular/common";
+import "@angular/platform-browser";
+import "@angular/platform-browser-dynamic";
+
+import "rxjs";
+
+//third party libs
+import "jquery";
+import "popper.js/dist/popper.min";
+import "bootstrap/dist/js/bootstrap";
+import "bootswatch/dist/united/bootstrap.min.css";
